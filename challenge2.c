f34ed486e02361a9dd321974a9980a6b604e91a6
mouad ziyani <ziyanimouad@gmail.com>
2025-09-05 15:53:42 +0100

    challenge2

---
+++ challenge2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main(){
+	
+	char c ;
+	
+	printf("entrer un charactere : ");
+    scanf("%c",&c);
+    
+    
+    switch (c){
+    case 'a':
+    	printf("une voyelle");
+    	break ;
+    case 'e':
+    	printf("une voyelle");
+    	break ;
+    case 'i':
+    	printf("une voyelle");
+    	break ;
+    case 'o':
+    	printf("une voyelle");
+    	break ;
+     case 'u':
+    	printf("une voyelle");
+    	break ;
+    case 'A':
+    	printf("une voyelle");
+    	break ;
+    case 'E':
+    	printf("une voyelle");
+    	break ;
+    case 'I':
+    	printf("une voyelle");
+    	break ;
+    case 'O':
+    	printf("une voyelle");
+    	break ;
+    case 'U':
+    	printf("une voyelle");
+    	break ;
+ 	  default:    
+    	printf("n'a pas une voyelle");
+    	break ;
+	} 
+	
+	return 0 ;
+}
