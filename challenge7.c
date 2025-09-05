da0ad3e0b23a69c4c8f1872efc4990d064902520
mouad ziyani <ziyanimouad@gmail.com>
2025-09-05 17:05:13 +0100

    challenge7

---
+++ challenge7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+
+    printf("Entrez un caractere : ");
+    scanf("%c", &c);
+
+    if (c >= 65 && c <= 90) {
+        printf("%c est une lettre majuscule.\n", c);
+    } else {
+        printf("%c n'est pas une lettre majuscule.\n", c);
+    }
+
+    return 0;
+}
+
