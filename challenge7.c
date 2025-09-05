21342f4c3e10892605f877ff18293867dc4f037b
mouad ziyani <ziyanimouad@gmail.com>
2025-09-05 19:50:24 +0100

    challenge7

--- challenge7.c
+++ challenge7.c
@@ -8,10 +8,11 @@ int main() {
 
     if (c >= 65 && c <= 90) {
         printf("%c est une lettre majuscule.\n", c);
+    } else if(c >= 97 && c <= 122) {
+        printf("%c est une lettre miniscule.\n", c);
     } else {
-        printf("%c n'est pas une lettre majuscule.\n", c);
+        printf("%c n'est pas une lettre .\n", c);
     }
-
     return 0;
 }
 
